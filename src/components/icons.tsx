03a528a86ea0fd1b8eb761e82dd9e6e622173587
Aria Farhad <Ariaemaan@gmail.com>
2025-07-16 18:30:38 +0000

    Still eroor

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,17 +1,28 @@
 
-import { Apple, Facebook } from 'lucide-react';
-
 export function GoogleIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-      <path d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12s4.48 10 10 10c2.76 0 5.26-1.12 7.07-2.93"/>
-      <path d="M22 12H12"/>
-      <path d="M12 2v10"/>
-      <path d="M12 12l-7.07 7.07"/>
-      <path d="M19.07 4.93L12 12"/>
-      <path d="M19.07 19.07L12 12"/>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 48 48" fill="none">
+        <path fillRule="evenodd" clipRule="evenodd" d="M48 24C48 22.0435 47.2104 20.1692 45.8284 18.7872C44.4465 17.4052 42.5722 16.6157 40.6157 16.6157H24V31.3843H37.4828C36.6974 34.2934 34.7893 36.7337 32.1834 38.1617L32.2217 38.2234L40.093 44.1833L40.6157 44.2217C45.0857 40.093 48 32.7217 48 24Z" fill="#4285F4"/>
+        <path fillRule="evenodd" clipRule="evenodd" d="M24 48C30.6157 48 36.2571 45.7337 40.6157 42.2217L32.1834 36.1617C29.9893 37.6692 27.2334 38.6157 24 38.6157C17.7428 38.6157 12.3809 34.3681 10.5186 28.7783L10.4357 28.9814L2.37833 35.143L2.22168 35.3843C6.46809 43.143 14.6157 48 24 48Z" fill="#34A853"/>
+        <path fillRule="evenodd" clipRule="evenodd" d="M10.5186 28.7783C10.0571 27.3843 9.77143 25.8828 9.77143 24.3C9.77143 22.7171 10.0571 21.2157 10.4803 19.8217L10.4389 19.6157L2.22168 13.2157C0.803483 16.2 0 19.993 0 24.3C0 28.6071 0.803483 32.4 2.22168 35.3843L10.5186 28.7783Z" fill="#FBBC05"/>
+        <path fillRule="evenodd" clipRule="evenodd" d="M24 9.38428C28.0107 9.38428 31.4286 10.7783 34.3315 13.4814L40.8357 7.22168C36.2201 3.09303 30.6157 0.600006 24 0.600006C14.6157 0.600006 6.46809 5.45698 2.22168 13.2157L10.4803 19.8217C12.3809 14.2319 17.7428 9.38428 24 9.38428Z" fill="#EA4335"/>
     </svg>
   );
 }
 
-export { Apple as AppleIcon, Facebook as FacebookIcon };
+export function AppleIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+       <path d="M12 20.9a9.6 9.6 0 0 0 8.5-5 9.5 9.5 0 0 0-8.5-5 9.5 9.5 0 0 0-8.5 5c0 .3.1.5.2.7a2.5 2.5 0 0 1-2.7 3.5 9.5 9.5 0 0 0 .2 4.3 9.6 9.6 0 0 0 8.3 4.5zM12 20.9a9.6 9.6 0 0 1-8.5-4.5 2.5 2.5 0 0 1 2.7-3.5c-.1-.2-.2-.4-.2-.7a9.5 9.5 0 0 1 8.5-5 9.5 9.5 0 0 1 8.5 5 9.6 9.6 0 0 1-8.5 5z"/>
+       <path d="M16 8.9a2 2 0 0 1-3-2.7 2.3 2.3 0 0 1 3 2.7z"/>
+    </svg>
+  );
+}
+
+export function FacebookIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"/>
+    </svg>
+  );
+}
