e4159e1a5e930f6c89f074cbb5e77b47c07945f0
Aria Farhad <Ariaemaan@gmail.com>
2025-07-16 19:18:49 +0000

    App not publish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,12 +31,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="theme-color" content="#D7263D" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body
         className={`${poppins.variable} ${inter.variable} font-body antialiased`}
